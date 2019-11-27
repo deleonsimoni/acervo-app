d781e0e9faa6c2e63cd12caf96134b37cf08b8bd
DeleonxD <deleonsimoni@gmail.com>
2019-11-27 01:30:56 -0200

    correções login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,5 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 
@@ -9,33 +9,41 @@ import { TooltipComponent } from '@angular/material';
 @Injectable()
 export class AuthService {
 
-  constructor(private http : HttpClient, private token: TokenStorage) {}
+  constructor(private http: HttpClient, private token: TokenStorage) { }
 
   public $userSource = new Subject<any>();
 
-  login(email : string, password : string) : Observable <any> {
+  login(email: string, password: string): Observable<any> {
     return Observable.create(observer => {
+
       this.http.post('/api/auth/login', {
         email,
         password
-      }).subscribe((data : any) => {
-          observer.next({user: data.user});
-          this.setUser(data.user);
-          this.token.saveToken(data.token);
-          observer.complete();
-      })
+      }).subscribe((data: any) => {
+        observer.next({ user: data.user });
+        this.setUser(data.user);
+        this.token.saveToken(data.token);
+        observer.complete();
+      }, error => {
+        if (error.status === 401) {
+          alert('Usuário ou senha inválidos')
+        } else {
+          alert('Ocorreu um erro na autenticação, tente novamente mais tarde.')
+        }
+      });
+
     });
   }
 
-  register(fullname : string, email : string, password : string, repeatPassword : string) : Observable <any> {
+  register(fullname: string, email: string, password: string, repeatPassword: string): Observable<any> {
     return Observable.create(observer => {
       this.http.post('/api/auth/register', {
         fullname,
         email,
         password,
         repeatPassword
-      }).subscribe((data : any) => {
-        observer.next({user: data.user});
+      }).subscribe((data: any) => {
+        observer.next({ user: data.user });
         this.setUser(data.user);
         this.token.saveToken(data.token);
         observer.complete();
@@ -56,9 +64,9 @@ export class AuthService {
   me(): Observable<any> {
     return Observable.create(observer => {
       const tokenVal = this.token.getToken();
-      if (!tokenVal) return  observer.complete();
-      this.http.get('/api/auth/me').subscribe((data : any) => {
-        observer.next({user: data.user});
+      if (!tokenVal) return observer.complete();
+      this.http.get('/api/auth/me').subscribe((data: any) => {
+        observer.next({ user: data.user });
         this.setUser(data.user);
         observer.complete();
       })
